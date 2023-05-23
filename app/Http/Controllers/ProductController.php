87b110c028dd482d897ff56e267e6a677a329768
Mahendra <mahendrachhetri500@gmail.com>
2023-05-23 20:48:06 +0545

    Change

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Categories;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -12,59 +11,50 @@ class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $products= Product::all();
+        $products = Product::all();
         return view('product.index',compact('products'));
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         $categories = Category::all();
         return view('product.create',compact('categories'));
-        
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         $data = $request->validate([
-            'category_id'=>'required',
-            'name'=>'required',
-            'price'=>'required|numeric',
-            'stock'=>'required',
-            'description'=>'required',
-            'photopath'=>'required'
+            'category_id' => 'required',
+            'name' => 'required',
+            'price' => 'numeric|required',
+            'stock' => 'numeric|required',
+            'description' => 'required',
+            'photopath' => 'required'
         ]);
+
         if($request->hasFile('photopath')){
             $image = $request->file('photopath');
             $name = time().'.'.$image->getClientOriginalExtension();
             $destinationPath = public_path('/images/products');
-            $image ->move($destinationPath,$name);
-            $data['photopath']= $name;
+            $image->move($destinationPath,$name);
+            $data['photopath'] = $name;
         }
+
         Product::create($data);
         return redirect(route('product.index'))->with('success','Product Created Successfully');
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
     {
@@ -73,9 +63,6 @@ class ProductController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
@@ -86,45 +73,40 @@ class ProductController extends Controller
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
         $product = Product::find($id);
         $data = $request->validate([
-            'category_id'=>'required',
-            'name'=>'required',
-            'price'=>'required|numeric',
-            'stock'=>'required|numeric',
-            'description'=>'required',
-            'photopath'=>'nullable'
+            'category_id' => 'required',
+            'name' => 'required',
+            'price' => 'numeric|required',
+            'stock' => 'numeric|required',
+            'description' => 'required',
+            'photopath' => 'nullable'
         ]);
+
         if($request->hasFile('photopath')){
             $image = $request->file('photopath');
             $name = time().'.'.$image->getClientOriginalExtension();
             $destinationPath = public_path('/images/products');
-            $image ->move($destinationPath,$name);
-            File::destroy(public_path('images/products/'.$product->photopath));
-            $data['photopath']= $name;
+            $image->move($destinationPath,$name);
+            File::delete(public_path('images/products/'.$product->photopath));
+            $data['photopath'] = $name;
         }
+
         $product->update($data);
-        return redirect(route('product.index'))->with('success','Product Update Successfully');
+        return redirect(route('product.index'))->with('success','Product Updated Successfully');
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        $product = Product::find($id);
+        $product = Product::find($request->dataid);
+        File::delete(public_path('images/products/'.$product->photopath));
         $product->delete();
-        File::delete(public_path('/images/product/'.$product->photopath));
         return redirect(route('product.index'))->with('success','Product Deleted Successfully');
     }
 }
